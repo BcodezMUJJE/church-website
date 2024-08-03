3922734f2fa9a15aabaa028da44968dcf4399317
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2024-08-03 13:02:57 +0000

    in and out

--- lighthouse grace church/src/Components/Programs/Program.jsx
+++ lighthouse grace church/src/Components/Programs/Program.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './Program.css'
-import Prog1 from '../../assets/'
+import Prog1 from '../../assets/praying-children.jpg'
+import Prog2 from '../../assets/married.jpg'
+import Prog3 from '../../assets/youth.jpg'
 
-const Programs = () {
+const Programs = () => {
     return (
         <div className='programs'>
             <div className="program">
-                <img src="" alt="" />
+                <img src={Prog1} alt="" />
+                <div className="caption">
+                    <p>Children's Service</p>
+                </div>
+            </div>
+            <div className="program">
+                <img src={Prog2} alt="" />
+                <div className="caption">
+                    <p>Married Fire Camp</p>
+                </div>
+            </div>
+            <div className="program">
+                <img src={Prog3} alt="" />
+                <div className="caption">
+                    <p>Youth Experience</p>
+                </div>
             </div>
         </div>
     )
